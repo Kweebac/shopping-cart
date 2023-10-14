4acb5d39e83c85923337ffc653a8107aca71543d
Kweebac <06.amarshall@gmail.com>
2023-10-14 15:35:44 +0100

    Skeleton files

---
+++ src/components/Router.jsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App/App";
+
+function Router() {
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <App />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default Router;
